7072a544af19f634ecc44e1fb827638ec161cbd5
Michael Thomas <michael.thomas@willowtreeapps.com>
2016-02-25 00:32:09 -0500

    More testing

--- swiftcurl.h
+++ swiftcurl.h
@@ -1,10 +1,24 @@
 #ifndef __SWIFTCURL_H
 #define __SWIFTCURL_H
 
-#include 
+// #include 
 #include "swiftcurl.h"
 
-CURLcode curl_easy_setopt_string(CURL *curl, CURLoption option, const char *param);
-CURLcode curl_easy_setopt_bool(CURL *curl, CURLoption option, bool param);
+// CURLcode curl_easy_setopt_string(CURL *curl, CURLoption option, const char *param);
+// CURLcode curl_easy_setopt_bool(CURL *curl, CURLoption option, bool param);
+
+// #include <curl/curl.h>
+// #include <stdbool.h>
+// #include <stdio.h>
+
+#define CURL_INLINE static __inline__
+
+CURL_INLINE CURLcode curl_easy_setopt_string(CURL *curl, CURLoption option, const char *param) {
+    return curl_easy_setopt(curl, option, param);
+}
+
+CURL_INLINE CURLcode curl_easy_setopt_bool(CURL *curl, CURLoption option, bool param) {
+    return curl_easy_setopt(curl, option, param);
+}
 
 #endif
